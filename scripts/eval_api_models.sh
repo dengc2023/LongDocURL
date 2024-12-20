9c5a9ab5f255e18b01615727c3d25e0af688d138
dc433561 <dc433561@taobao.com>
2024-12-20 19:52:56 +0800

    basic commit

---
+++ scripts/eval_api_models.sh
@@ -0,0 +1,5 @@
+python eval/api_models/eval_api_models.py \
+    --qa_file data/LongDocURL.jsonl \
+    --results_file evaluation_results/api_models/results_gpt4o.jsonl \
+    --process_mode parallel \
+    --model_name gpt4o
